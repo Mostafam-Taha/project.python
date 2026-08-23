632a7c04b228ce7085d74f96a736e1e9700c0797
mostafam taha <mostafamta347@gmail.com>
2026-08-23 18:56:06 +0300

    Add files via upload

--- Data Structures/Binary Search.py
+++ Data Structures/Binary Search.py
@@ -1,2 +1,21 @@
 # Binary Search
+def binarysearch(array, search, lowindex, highindex):
+    while lowindex <= highindex:
+        avg = lowindex + (highindex - lowindex) // 2
+
+        if array[avg] == search:
+            return avg
+        elif array[avg] < search:
+            return avg + 1
+        else:
+            return avg - 1
+    return -1
 numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+search = 20
+
+result = binarysearch(numbers, search, 0, len(numbers) - 1)
+
+if result == -1:
+    print("Not found")
+else:
+    print("Found")
